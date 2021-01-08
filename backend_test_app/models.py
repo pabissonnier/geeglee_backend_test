75e63279698a7afe0f638e27a79bc070ba2eb75f
pabissonnier <pabissonnier@gmail.com>
2021-01-08 14:34:12 +0100

    Models Customer, Company and licence created (os import on settings as well)

--- backend_test_app/models.py
+++ backend_test_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Licence(models.Model):
+    type = models.CharField(max_length=200, null=False)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    company = models.OneToOneField("Company", on_delete=models.CASCADE, related_name='licence_company')
+
+    def __str__(self):
+        return self.type
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    area = models.CharField(max_length=200, null=False)
+    nb_employees = models.IntegerField(null=False)
+    licence = models.ForeignKey(Licence, on_delete=models.CASCADE, related_name='company_licence')
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    fname = models.CharField(max_length=200, null=False)
+    lname = models.CharField(max_length=200, null=False)
+    email = models.EmailField(max_length=200, null=False)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.email
