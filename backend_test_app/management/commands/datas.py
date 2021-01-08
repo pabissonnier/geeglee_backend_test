a5df9912730c0429ea8f31c1451ee007fcf84e13
pabissonnier <pabissonnier@gmail.com>
2021-01-08 14:54:15 +0100

    Adding some datas to fill the db through custom django-admin commands

---
+++ backend_test_app/management/commands/datas.py
@@ -0,0 +1,80 @@
+customer = [
+    {
+        'fname': 'jacques',
+        'lname': 'antoine',
+        'email': 'ja@total.com',
+        'company': 'total',
+    },
+    {
+        'fname': 'yasmine',
+        'lname': 'kazou',
+        'email': 'yk@engie.com',
+        'company': 'engie',
+    }
+]
+
+licence = [
+    {
+        'type': 'full',
+        'start_date': '2020-01-01 00:00:00',
+        'company': "psa"
+    },
+    {
+        'type': 'full',
+        'start_date': '2020-01-01 00:00:00',
+        'company': "total"
+    },
+    {
+        'type': 'Full',
+        'start_date': '2020-01-01 00:00:00',
+        'company': "engie"
+    },
+    {
+        'type': 'Light',
+        'start_date': '2020-01-01 00:00:00',
+        'company': "dassault"
+    },
+    {
+        'type': 'Full',
+        'start_date': '2020-01-01 00:00:00',
+        'company': "airbus"
+    },
+    {
+        'type': 'Full',
+        'start_date': '2020-01-01 00:00:00',
+        'company': "sncf"
+    },
+]
+
+company = {
+    'total': {
+        'area': 'parapétrolier',
+        'employees': 50000,
+        'licence': 2,
+    },
+    'engie': {
+        'area': 'énergie',
+        'employees': 40000,
+        'licence': 3,
+    },
+    'psa': {
+        'area': 'automobile',
+        'employees': 70000,
+        'licence': 1,
+    },
+    'airbus': {
+        'area': 'aéronautique',
+        'employees': 35000,
+        'licence': 5,
+    },
+    'sncf': {
+        'area': 'ferroviaire',
+        'employees': 15000,
+        'licence': 6,
+    },
+    'dassault': {
+        'area': 'défense',
+        'employees': 5000,
+        'licence': 4,
+    },
+}
