690af91333d119b74a3c57848b8f25d933db6694
pabissonnier <pabissonnier@gmail.com>
2021-01-08 16:02:39 +0100

    viewsets ok

--- backend_test_app/views.py
+++ backend_test_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CompanySerializer, CustomerSerializer, LicenceSerializer
+from .models import Company, Customer, Licence
 
-# Create your views here.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all().order_by('name')
+    serializer_class = CompanySerializer
+
+
+class LicenceViewSet(viewsets.ModelViewSet):
+    queryset = Licence.objects.all().order_by('type')
+    serializer_class = LicenceSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all().order_by('lname')
+    serializer_class = CustomerSerializer
