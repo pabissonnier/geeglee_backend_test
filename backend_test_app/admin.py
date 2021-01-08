199ef11a6cba9d7ac5c1917703e9092d5791ace9
pabissonnier <pabissonnier@gmail.com>
2021-01-08 14:38:42 +0100

    Models added to admin.py

--- backend_test_app/admin.py
+++ backend_test_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Company, Customer, Licence
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Company)
+admin.site.register(Licence)
